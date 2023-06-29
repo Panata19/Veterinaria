40d6673846fa57b2905c890c118ab0fa7d5275be
Anthony Rodríguez <rodriguezanthony981@gmail.com>
2023-06-29 11:53:20 -0500

    tabla funciona

--- src/app/modules/inventario/bodega/pages/main/bodega.component.ts
+++ src/app/modules/inventario/bodega/pages/main/bodega.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-//import DataTable from 'datatables.net-dt';
 import DataTable from 'datatables.net-bs4';
-import 'datatables.net-buttons-bs4';
-import 'datatables.net-buttons/js/buttons.html5.mjs';
-import 'datatables.net-colreorder-bs4';
-import 'datatables.net-responsive-bs4';
-import 'datatables.net-scroller-bs4';
-import 'datatables.net-searchpanes-bs4';
-import 'datatables.net-select-bs4';
+
 
 @Component({
   selector: 'app-bodega',
@@ -20,18 +13,33 @@ export class BodegaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
     let table = new DataTable('#myTable', {
+      paging: true,
+      ordering: true,
+      info: true,
+      lengthMenu: [
+        [5,10, 25, 50, -1],
+        [5,10, 25, 50, 'All'],
+      ],
       
-    });
+  });
     
   }
-  /** 
-   * npm install --save datatables.net-bs4
-    npm install --save datatables.net-buttons-bs4
+  /**
+    npm install --save jquery
+    npm install --save datatables.net-bs4
+    npm install --save datatables.net-keytable-bs4
     npm install --save datatables.net-responsive-bs4
-    npm install --save datatables.net-scroller-bs4
     npm install --save datatables.net-searchpanes-bs4
     npm install --save datatables.net-select-bs4
    * 
-   * **/
+   * 
+   import jQuery from "jquery";
+   import DataTable from 'datatables.net-bs4';
+   import 'datatables.net-keytable-bs4';
+   import 'datatables.net-responsive-bs4';
+   import 'datatables.net-searchpanes-bs4';
+   import 'datatables.net-select-bs4';
+   * */ 
 }
