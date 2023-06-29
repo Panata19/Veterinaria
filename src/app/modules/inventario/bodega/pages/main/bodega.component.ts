d6220af7ae781fc1418c85d4287cde28571fe5b4
Anthony Rodríguez <rodriguezanthony981@gmail.com>
2023-06-29 15:19:07 -0500

    Empezando uso de material

--- src/app/modules/inventario/bodega/pages/main/bodega.component.ts
+++ src/app/modules/inventario/bodega/pages/main/bodega.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import DataTable from 'datatables.net-bs4';
 
 
 @Component({
@@ -13,26 +12,7 @@ export class BodegaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    let table = new DataTable('#myTable', {
-      paging: true,
-      ordering: true,
-      info: true,
-      lengthMenu: [
-        [5,10, 25, 50, -1],
-        [5,10, 25, 50, 'All'],
-      ],
-      
-     });
     
   }
-  /**
-    npm install --save jquery
-    npm install --save datatables.net-bs4
-    npm install --save datatables.net-responsive-bs4
-   * 
-   * 
-   import DataTable from 'datatables.net-bs4';
-   import 'datatables.net-keytable-bs4';
-   import 'datatables.net-responsive-bs4';
-   * */ 
+  
 }
