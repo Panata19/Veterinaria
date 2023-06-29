0faf9fca04c3be0c9f1fe6774ec168c36da98486
Anthony Rodríguez <rodriguezanthony981@gmail.com>
2023-06-29 14:53:42 -0500

    Final usando la libreria, comenzare a usar angular material

--- src/app/modules/inventario/bodega/pages/main/bodega.component.ts
+++ src/app/modules/inventario/bodega/pages/main/bodega.component.ts
@@ -13,7 +13,6 @@ export class BodegaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
     let table = new DataTable('#myTable', {
       paging: true,
       ordering: true,
@@ -23,23 +22,17 @@ export class BodegaComponent implements OnInit {
         [5,10, 25, 50, 'All'],
       ],
       
-  });
+     });
     
   }
   /**
     npm install --save jquery
     npm install --save datatables.net-bs4
-    npm install --save datatables.net-keytable-bs4
     npm install --save datatables.net-responsive-bs4
-    npm install --save datatables.net-searchpanes-bs4
-    npm install --save datatables.net-select-bs4
    * 
    * 
-   import jQuery from "jquery";
    import DataTable from 'datatables.net-bs4';
    import 'datatables.net-keytable-bs4';
    import 'datatables.net-responsive-bs4';
-   import 'datatables.net-searchpanes-bs4';
-   import 'datatables.net-select-bs4';
    * */ 
 }
