a9846a2e0b7c4bf6e72b3ac17e8b0d0afe00f335
Anthony Rodríguez <rodriguezanthony981@gmail.com>
2023-06-29 09:21:48 -0500

    Pruebas en Tablas

--- src/app/modules/inventario/bodega/pages/main/bodega.component.ts
+++ src/app/modules/inventario/bodega/pages/main/bodega.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+//import DataTable from 'datatables.net-dt';
+import DataTable from 'datatables.net-bs4';
+import 'datatables.net-buttons-bs4';
+import 'datatables.net-buttons/js/buttons.html5.mjs';
+import 'datatables.net-colreorder-bs4';
+import 'datatables.net-responsive-bs4';
+import 'datatables.net-scroller-bs4';
+import 'datatables.net-searchpanes-bs4';
+import 'datatables.net-select-bs4';
 
 @Component({
   selector: 'app-bodega',
@@ -11,6 +20,18 @@ export class BodegaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let table = new DataTable('#myTable', {
+      
+    });
+    
   }
-
+  /** 
+   * npm install --save datatables.net-bs4
+    npm install --save datatables.net-buttons-bs4
+    npm install --save datatables.net-responsive-bs4
+    npm install --save datatables.net-scroller-bs4
+    npm install --save datatables.net-searchpanes-bs4
+    npm install --save datatables.net-select-bs4
+   * 
+   * **/
 }
