f7cfc2a2e7261f71d5384b796faa4cfe676223c7
bryanjacomet2001 <bj2257239@gmail.com>
2023-07-29 13:24:23 -0500

    Añadiendo el objeto de FiltroHospitalizacion

--- src/app/shared/config/config.ts
+++ src/app/shared/config/config.ts
@@ -1,9 +1,11 @@
 export const tipofiltros  = Object.freeze(
   {
     ''              : { tipo: 'text', placeholder: 'Buscar...', disable: true }, 
-    'nombrePaciente': { tipo: 'text', placeholder: 'Buscar Paciente', disable: false},
-    'fechaNac'      : { tipo: 'date', nombre: 'fechaNac', disable: false },
-    'fechaIngreso'  : { tipo: 'date', nombre: 'fechaIngreso', disable: false },
-    'todos'         : { tipo: '', placeholder: '', disable: true}
+    'NOMBRE_PACIENTE': { tipo: 'text', placeholder: 'Buscar Paciente', disable: false},
+    'FECHA_NAC'      : { tipo: 'date', nombre: 'fechaNac', disable: false },
+    'FECHA_INGRESO'  : { tipo: 'date', nombre: 'fechaIngreso', disable: false },
+    'TODOS'         : { tipo: '', placeholder: '', disable: true}
   }
 );
+
+
