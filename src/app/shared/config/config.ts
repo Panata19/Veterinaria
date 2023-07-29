cfba259221d8953ebfe85fa5864ce2d299a68867
bryanjacomet2001 <bj2257239@gmail.com>
2023-07-29 13:29:35 -0500

    Agregando una mejor estructura al objeto TIPO_FILTROS

--- src/app/shared/config/config.ts
+++ src/app/shared/config/config.ts
@@ -1,10 +1,10 @@
-export const tipofiltros  = Object.freeze(
+export const TIPO_FILTROS  = Object.freeze(
   {
-    ''              : { tipo: 'text', placeholder: 'Buscar...', disable: true }, 
+    ''              :  { tipo: 'text', placeholder: 'Buscar...', disable: true }, 
     'NOMBRE_PACIENTE': { tipo: 'text', placeholder: 'Buscar Paciente', disable: false},
     'FECHA_NAC'      : { tipo: 'date', nombre: 'fechaNac', disable: false },
     'FECHA_INGRESO'  : { tipo: 'date', nombre: 'fechaIngreso', disable: false },
-    'TODOS'         : { tipo: '', placeholder: '', disable: true}
+    'TODOS'         :  { tipo: '',     placeholder: '', disable: true}
   }
 );
 
